2f78df9d709c7d8580c4ae552a6b5239c8bdccc6
Matthew Vilim <mvilim@stanford.edu>
2018-10-09 16:09:33 -0700

    i

--- fringe/src/fringe/templates/streamarbiter/StreamControllerLoad.scala
+++ fringe/src/fringe/templates/streamarbiter/StreamControllerLoad.scala
@@ -4,16 +4,16 @@ import chisel3._
 import chisel3.util._
 import fringe._
 
-class LoadStreamControllerIO(dramStream: DRAMStream, loadStream: LoadStream) extends StreamControllerIO(dramStream) {
+class StreamControllerLoadIO(dramStream: DRAMStream, loadStream: LoadStream) extends StreamControllerIO(dramStream) {
   val load = loadStream.cloneType
   val sizeCounterDone = Bool()
 }
 
-class LoadStreamController(
+class StreamControllerLoad(
   dramStream: DRAMStream,
   loadStream: LoadStream
 ) extends StreamController(dramStream) (
-  val io = IO(new LoadStreamControllerIO(dramStream, loadStream)
+  val io = IO(new StreamControllerLoadIO(dramStream, loadStream)
 
   val rdata = Module(new FIFOWidthConvert(external_w, io.dram.rresp.bits.rdata.size, info.w, info.v, d))
   rdata.io.enq := io.dram.rresp.bits.rdata
--- fringe/src/fringe/templates/streamarbiter/StreamControllerStore.scala
+++ fringe/src/fringe/templates/streamarbiter/StreamControllerStore.scala
@@ -4,13 +4,13 @@ import chisel3._
 import chisel3.util._
 import fringe._
 
-class StoreStreamControllerIO(dramStream: DRAMStream, storeStream: StoreStream) extends StreamControllerIO(dramStream) {
+class StreamControllerStoreIO(dramStream: DRAMStream, storeStream: StoreStream) extends StreamControllerIO(dramStream) {
   val store = storeStream.cloneType()
 }
 
-class StoreStreamController(
+class StreamControllerStore(
   dramStream: DRAMStream,
   storeStream: StoreStream
 ) extends StreamController(dramStream) {
-  val io = IO(new StoreStreamControllerIO(dramStream, storeStream)
+  val io = IO(new StreamControllerStoreIO(dramStream, storeStream)
 }
