3914c72f6184ab1038e819c071d59b26448e06fa
Matthew Vilim <mvilim@stanford.edu>
2018-10-10 15:43:21 -0700

    i

--- fringe/src/fringe/templates/streamarbiter/StreamController.scala
+++ fringe/src/fringe/templates/streamarbiter/StreamController.scala
@@ -32,13 +32,13 @@ class StreamControllerLoad(
   val io = IO(new StreamControllerLoadIO)
 
   val cmd = Module(new FIFO(app.cmd.bits, depth))
-  cmd.in.valid := io.load.cmd.valid
+  cmd.io.in.valid := io.load.cmd.valid
   io.load.cmd.ready := cmd.io.in.ready
   cmd.io.out.ready := io.dram.cmd.ready
   io.dram.cmd.valid := cmd.io.out.valid
   
-  io.dram.cmd.bits.addr := cmd.io.out.addr
-  io.dram.cmd.bits.rawAddr := cmd.io.out.addr
+  io.dram.cmd.bits.addr := cmd.io.out.bits.addr
+  io.dram.cmd.bits.rawAddr := cmd.io.out.bits.addr
   io.dram.cmd.bits.size := cmd.io.out.size
   io.dram.cmd.bits.isWr := false.B
 
@@ -66,7 +66,7 @@ class StreamControllerStore(
   val io = IO(new StreamControllerStoreIO)
 
   val cmd = Module(new FIFO(app.cmd.bits, depth))
-  cmd.in.valid := io.store.cmd.valid
+  cmd.io.in.valid := io.store.cmd.valid
   io.store.cmd.ready := cmd.io.in.ready
   cmd.io.out.ready := io.dram.cmd.ready
   io.dram.cmd.valid := cmd.io.out.valid
@@ -77,10 +77,25 @@ class StreamControllerStore(
   io.dram.cmd.bits.isWr := true.B
 
   val wdata = Module(new FIFOWidthConvert(info.w, info.v, externalW, externalV, depth))
-  wdata.io.in.bits := io.dram.rresp.bits.rdata
-  wdata.io.in.valid := io.dram.rresp.valid
+  wdata.io.in.valid := io.store.wdata.valid
+  wdata.io.in.bits.data := io.store.wdata.bits
+  wdata.io.in.bits.strobe := io.store.wstrb.bits
+  io.store.wdata.ready := wdata.io.in.ready
+  io.store.wstrb.ready := wdata.io.in.ready
 
-  io.dram.rresp.ready := ~rdata.io.full
+  io.dram.wdata.valid := wdata.io.out.valid
+  io.dram.wdata.bits.wdata := wdata.io.out.data
+  io.dram.wdata.bits.wstrb := wdata.io.out.strobe
+  wdata.io.out.ready := io.dram.wdata.ready
+
+  val wresp = Module(new FIFO(Bool(), depth))
+  wresp.io.in.valid := io.dram.wresp.valid
+  wresp.io.in.bits := io.dram.wresp.valid
+  io.dram.wresp.ready := wresp.io.in.ready
+
+  io.store.wresp.valid := wresp.io.out.valid
+  io.store.wresp.bits := wresp.io.out.bits
+  wresp.io.out.ready := io.store.wresp.ready
 }
 
 class StreamControllerGather(
