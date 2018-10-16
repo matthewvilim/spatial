6d45f23a00c0307191760954ec29174251ffc9b0
Matthew Vilim <mvilim@stanford.edu>
2018-10-15 17:48:29 -0700

    i

--- fringe/src/fringe/templates/streamarbiter/StreamArbiter.scala
+++ fringe/src/fringe/templates/streamarbiter/StreamArbiter.scala
@@ -31,9 +31,8 @@ class StreamArbiter(
     val reset = Input(Bool())
     val app = new AppStreams(loadStreamInfo, storeStreamInfo, gatherStreamInfo, scatterStreamInfo)
     val dram = new DRAMStream(EXTERNAL_W, EXTERNAL_V)
-    val debugSignals = Output(Vec(numDebugs, UInt(32.W)))
 
-    // AXI Debuggers
+    val debugSignals = Output(Vec(numDebugs, UInt(32.W)))
     val TOP_AXI = new AXI4Probe(axiLiteParams)
     val DWIDTH_AXI = new AXI4Probe(axiLiteParams)
     val PROTOCOL_AXI = new AXI4Probe(axiLiteParams)
@@ -85,8 +84,131 @@ class StreamArbiter(
   io.dram.cmd.valid := io.enable & axiIssue.io.out.cmd.valid
   io.dram.wdata.valid := io.enable & axiIssue.io.out.wdata.valid
 
+  // debug
   if (isDebugChannel) {
-		val signalLabels = ListBuffer[String]()
+    var dbgCount = 0
+    val signalLabels = ListBuffer[String]()
+    def connectDbgSig(sig: UInt, label: String): Unit = {
+      if (globals.enableDebugRegs) {
+        if (isDebugChannel) {
+          io.debugSignals(dbgCount) := sig
+          val padded_label = if (label.length < 55) {label + "."*(55-label.length)} else label
+          signalLabels.append(padded_label)
+          dbgCount += 1
+        }
+      }
+    }
+
+    def debugCounter(en: Bool) = {
+      val c = RegInit(0.U(64.W))
+      when (en) {
+        c := c + 1.U
+      }
+      c
+    }
+
+    def debugFF[T<:Data](sig: T, en: Bool) = {
+      val ff = RegInit(sig)
+      when (en) {
+        ff := sig
+      }
+      ff
+    }
+
+    val cycleCount = debugCounter(io.enable)
+    connectDbgSig(cycleCount, "Cycles")
+
+    val rdataEnqCount = debugCounter(io.dram.rresp.valid & io.dram.rresp.ready)
+    val wdataCount = debugCounter(io.dram.wdata.valid & io.dram.wdata.ready)
+    connectDbgSig(wdataCount, "[FRINGE] num wdata transferred (wvalid & wready)")
+
+    connectDbgSig(debugCounter(io.dram.cmd.ready & io.dram.cmd.valid), "[FRINGE] # DRAM Commands Issued")
+    connectDbgSig(debugCounter(io.dram.cmd.ready & io.dram.cmd.valid & !io.dram.cmd.bits.isWr), "[FRINGE] # Read Commands Sent")
+
+    // Count number of load commands issued from accel per stream
+    io.app.loads.zipWithIndex.foreach { case (load, i) =>
+      val loadCounterHandshake = debugCounter(io.enable & load.cmd.valid & load.cmd.ready)
+      connectDbgSig(loadCounterHandshake, s"[ACCEL] # from load stream $i")
+      val signal = s"[FRINGE] # from load stream $i"
+    }
+    // Count number of store commands issued from accel per stream
+    io.app.stores.zipWithIndex.foreach { case (store, i) =>
+      val storeCounterHandshake = debugCounter(io.enable & store.cmd.valid & store.cmd.ready)
+      connectDbgSig(storeCounterHandshake, s"[ACCEL] # from store stream $i")
+    }
+
+    connectDbgSig(debugCounter(io.dram.rresp.valid & io.dram.rresp.ready), "[FRINGE] # Read Responses Acknowledged")
+    connectDbgSig(debugCounter(io.dram.rresp.valid & !io.dram.rresp.ready), "[FRINGE] # RResp rejected by ready")
+    connectDbgSig(debugCounter(!io.dram.rresp.valid & io.dram.rresp.ready), "[FRINGE] Cycles RResp ready and idle (~valid)")
+    loadStreamInfo.indices.foreach{i =>
+      val signal = s"[FRINGE]  # from load stream $i"
+      connectDbgSig(debugCounter(io.dram.rresp.valid & io.dram.rresp.ready & (io.dram.rresp.bits.getTag.streamID === i.U)), signal)
+    }
+    connectDbgSig(debugCounter(io.dram.wresp.valid & io.dram.wresp.ready), "[FRINGE] # Write Responses Acknowledged")
+    connectDbgSig(debugCounter(io.dram.wresp.valid & !io.dram.wresp.ready), "[FRINGE] # WResp rejected by ready")
+    connectDbgSig(debugCounter(!io.dram.wresp.valid & io.dram.wresp.ready), "[FRINGE] Cycles WResp ready and idle (~valid)")
+    storeStreamInfo.indices.foreach{i =>
+      val signal = s"[FRINGE]  # from store stream $i"
+      connectDbgSig(debugCounter(io.dram.wresp.valid & io.dram.wresp.ready & (io.dram.wresp.bits.getTag.streamID === (i+loadStreamInfo.length).U)), signal)
+    }
+
+    // Connect AXI loopback debuggers
+    // TOP
+    connectDbgSig(debugCounter(io.TOP_AXI.ARVALID), "# cycles TOP ARVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.ARREADY), "# cycles TOP ARREADY")
+    connectDbgSig(debugCounter(io.TOP_AXI.ARREADY & io.TOP_AXI.ARVALID), "# cycles TOP ARREADY & ARVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.AWVALID), "# cycles TOP AWVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.AWREADY & io.TOP_AXI.AWVALID), "# cycles TOP AWREADY & AWVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.RVALID), "# cycles TOP RVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.RREADY & io.TOP_AXI.RVALID), "# cycles TOP RREADY & RVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.WVALID), "# cycles TOP WVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.WREADY & io.TOP_AXI.WVALID), "# cycles TOP WREADY & WVALID ")
+    connectDbgSig(debugCounter(!io.TOP_AXI.WREADY & io.TOP_AXI.WVALID), "# cycles TOP ~WREADY & WVALID (forced)" )
+    connectDbgSig(debugCounter(!io.TOP_AXI.WREADY), "# cycles TOP ~WREADY" )
+    connectDbgSig(debugCounter(io.TOP_AXI.BVALID), "# cycles TOP BVALID ")
+    connectDbgSig(debugCounter(io.TOP_AXI.BREADY & io.TOP_AXI.BVALID), "# cycles TOP BREADY & BVALID ")
+    connectDbgSig(debugFF(io.TOP_AXI.ARADDR, io.TOP_AXI.ARVALID & io.TOP_AXI.ARREADY), "Last TOP ARADDR")
+    connectDbgSig(debugFF(io.TOP_AXI.ARLEN, io.TOP_AXI.ARVALID & io.TOP_AXI.ARREADY), "Last TOP ARLEN")
+    connectDbgSig(debugFF(io.TOP_AXI.ARSIZE, io.TOP_AXI.ARVALID & io.TOP_AXI.ARREADY), "Last TOP ARSIZE")
+    connectDbgSig(debugFF(io.TOP_AXI.ARID, io.TOP_AXI.ARVALID & io.TOP_AXI.ARREADY), "Last TOP ARID")
+    connectDbgSig(debugFF(io.TOP_AXI.ARBURST, io.TOP_AXI.ARVALID & io.TOP_AXI.ARREADY), "Last TOP ARBURST")
+    connectDbgSig(debugFF(io.TOP_AXI.AWADDR, io.TOP_AXI.AWVALID & io.TOP_AXI.AWREADY), "Last TOP AWADDR")
+    connectDbgSig(debugFF(io.TOP_AXI.AWLEN, io.TOP_AXI.AWVALID & io.TOP_AXI.AWREADY), "Last TOP AWLEN")
+    connectDbgSig(debugFF(io.TOP_AXI.WDATA, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY), "Last TOP WDATA")
+    connectDbgSig(debugFF(io.TOP_AXI.WSTRB, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY), "Last TOP WSTRB")
+    connectDbgSig(debugFF(io.TOP_AXI.WDATA, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY & wdataCount === 0.U), "First TOP WDATA")
+    connectDbgSig(debugFF(io.TOP_AXI.WSTRB, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY & wdataCount === 0.U), "First TOP WSTRB")
+    connectDbgSig(debugFF(io.TOP_AXI.WDATA, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY & wdataCount === 1.U), "Second TOP WDATA")
+    connectDbgSig(debugFF(io.TOP_AXI.WSTRB, io.TOP_AXI.WVALID & io.TOP_AXI.WREADY & wdataCount === 1.U), "Second TOP WSTRB")
+
+    // // DWIDTH
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.ARVALID), "# cycles DWIDTH ARVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.ARREADY), "# cycles DWIDTH ARREADY ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.ARREADY & io.DWIDTH_AXI.ARVALID), "# cycles DWIDTH ARREADY & ARVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.AWVALID), "# cycles DWIDTH AWVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.AWREADY & io.DWIDTH_AXI.AWVALID), "# cycles DWIDTH ARREADY & AWVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.RVALID), "# cycles DWIDTH RVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.RREADY & io.DWIDTH_AXI.RVALID), "# cycles DWIDTH RREADY & RVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.WVALID), "# cycles DWIDTH WVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.WREADY & io.DWIDTH_AXI.WVALID), "# cycles DWIDTH WREADY & WVALID ")
+    connectDbgSig(debugCounter(!io.DWIDTH_AXI.WREADY & io.DWIDTH_AXI.WVALID), "# cycles DWIDTH ~WREADY & WVALID (forced)" )
+    connectDbgSig(debugCounter(!io.DWIDTH_AXI.WREADY), "# cycles DWIDTH ~WREADY" )
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.BVALID), "# cycles DWIDTH BVALID ")
+    connectDbgSig(debugCounter(io.DWIDTH_AXI.BREADY & io.DWIDTH_AXI.BVALID), "# cycles DWIDTH BREADY & BVALID ")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.ARADDR, io.DWIDTH_AXI.ARVALID & io.DWIDTH_AXI.ARREADY), "Last DWIDTH ARADDR")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.ARLEN, io.DWIDTH_AXI.ARVALID & io.DWIDTH_AXI.ARREADY), "Last DWIDTH ARLEN")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.ARSIZE, io.DWIDTH_AXI.ARVALID & io.DWIDTH_AXI.ARREADY), "Last DWIDTH ARSIZE")
+    // connectDbgSig(debugFF(io.DWIDTH_AXI.ARID, io.DWIDTH_AXI.ARVALID & io.DWIDTH_AXI.ARREADY), "Last DWIDTH ARID")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.ARBURST, io.DWIDTH_AXI.ARVALID & io.DWIDTH_AXI.ARREADY), "Last DWIDTH ARBURST")
+    // connectDbgSig(debugCounter(io.DWIDTH_AXI.ARLOCK), "# cycles DWIDTH ARLOCK ")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.AWADDR, io.DWIDTH_AXI.AWVALID & io.DWIDTH_AXI.AWREADY), "Last DWIDTH AWADDR")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.AWLEN, io.DWIDTH_AXI.AWVALID & io.DWIDTH_AXI.AWREADY), "Last DWIDTH AWLEN")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WDATA, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY), "Last DWIDTH WDATA")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WSTRB, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY), "Last DWIDTH WSTRB")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WDATA, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY & wdataCount === 0.U), "First DWIDTH WDATA")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WSTRB, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY & wdataCount === 0.U), "First DWIDTH WSTRB")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WDATA, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY & wdataCount === 1.U), "Second DWIDTH WDATA")
+    connectDbgSig(debugFF(io.DWIDTH_AXI.WSTRB, io.DWIDTH_AXI.WVALID & io.DWIDTH_AXI.WREADY & wdataCount === 1.U), "Second DWIDTH WSTRB")
 
     // Print all debugging signals into a header file
     val debugFileName = "cpp/generated_debugRegs.h"
