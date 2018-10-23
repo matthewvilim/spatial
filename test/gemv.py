a6d6d7236e3965b42b52d2922aaa70dcc7046273
Matthew Vilim <mvilim@stanford.edu>
2018-10-23 15:13:44 -0700

    i

--- test/gemv.py
+++ test/gemv.py
@@ -2,7 +2,11 @@ import numpy as np
 
 n = 1024
 
-matrix = np.random.rand(n,n)
-vector = np.random.rand(n)
-np.savetxt("matrix.csv", matrix, delimiter=",")
-np.savetxt("vector.csv", vector, delimiter=",")
+matrix = np.random.randint(200, size=(n,n))
+vector = np.random.randint(200, size=n)
+gold = np.matmul(matrix, vector)
+np.savetxt("matrix.csv", matrix, fmt="%i", delimiter=",")
+np.savetxt("vector.csv", vector, fmt="%i", delimiter=",")
+np.savetxt("gold.csv", gold, fmt="%i", delimiter=",")
+
+
