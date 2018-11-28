961bf1146e0259516f4ab5bcbff1ccfe7237be42
Matthew Vilim <mvilim@stanford.edu>
2018-11-28 13:23:52 -0800

    i

--- fringe/src/fringe/templates/vector/Shuffle.scala
+++ fringe/src/fringe/templates/vector/Shuffle.scala
@@ -1,37 +1,56 @@
 package fringe.templates.vector
 
 import chisel3._
+import chisel3.util._
 
-class ShuffleCompressNetwork[T <: Data](data: Vec[T]) extends Module {
+import fringe._
+import fringe.utils._
+
+class ShuffleCompressNetwork[T <: Data](t: T, v: Int) extends Module {
+  class Data extends Bundle {
+    val d = t.cloneType
+    val m = Bool()
+  }
+
   val io = IO(new Bundle {
-    val dataIn = Input(data.cloneType)
-    val maskIn = Input(Vec(data.length, Bool()))
-    val dataOut = Output(data.cloneType)
-    val maskOut = Output(Vec(data.length, Bool()))
+    val in = Input(Vec(v, new Data))
+    val out = Output(Vec(v, new Data))
   })
 
-  def level0(v: Int) = {
+  def levelEven(v: Int) = {
     List.range(0, v).sliding(2, 2).map { case List(a, b) => (a, b) }
   }
 
-  def level1(v: Int) = {
-    (0, 0) ++ List.range(1, v - 1).sliding(2, 2).map { case List(a, b) => (a, b) } ++ (v - 1, v - 1)
+  def levelOdd(v: Int) = {
+    (0, 0) +: List.range(1, v - 1).sliding(2, 2).map { case List(a, b) => (a, b) }.toList :+ (v - 1, v - 1)
   }
 
-  def compressLevel(data: Vec[T], mask: Vec[Bool]) = {
+  
+  val stages = List.fill(v) { Wire(Vec(v, new Data)) }
+
+  val stageMap = List.tabulate(v) { i => if (i % 2 == 0) levelEven(v) else levelOdd(v) }
+  stageMap.zipWithIndex.foreach { case (stage, i) =>
+    val in = if (i == 0) io.in else stages(i - 1)
+    val out = Vec(stage.flatMap { case (a, b) =>
+      val s = in(a).m
+      if (a == b) List(in(a)) else List(
+        Mux(s, in(a), in(b)),
+        Mux(s, in(b), in(a))
+      )
+    }.toList)
+    stages(i) := getRetimed(out, 1)
   }
 
-  val (d, m) = compressLevel(io.dataIn, io.maskIn)
-
-  io.dataOut := d
-  io.maskOut := m
+  io.out := stages.last
 }
 
 object Shuffle {
   def compress[T <: Data](data: Vec[T], mask: Vec[Bool]) = {
-    val m = Module(new ShuffleCompressNetwork(data))
-    m.io.dataIn := data
-    m.io.maskIn := mask
-    (m.io.dataOut, m.io.maskOut)
+    val m = Module(new ShuffleCompressNetwork(data(0), data.length))
+    m.io.in.zipWithIndex.foreach { case(in, i) =>
+      in.d := data(i)
+      in.m := mask(i)
+    }
+    (Vec(m.io.out.map { _.d }), Vec(m.io.out.map { _.d }))
   }
 }
