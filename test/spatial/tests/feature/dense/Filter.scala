448c78f21958e0cf4d4553ca6863c3c7e607a091
Matthew Vilim <mvilim@stanford.edu>
2018-12-07 16:17:05 -0800

    i

--- test/spatial/tests/feature/dense/Filter.scala
+++ test/spatial/tests/feature/dense/Filter.scala
@@ -8,9 +8,9 @@ import spatial.dsl._
     val n = 128
 
     val data = Array.tabulate(n){ i => i.to[Int] }
-    val mask = Array.tabulate(n){ i => i % 2 == 0 }
+    val mask = Array.tabulate(n){ i => (i % 2 == 0).to[Int] }
     val dataDRAM = DRAM[Int](n)
-    val maskDRAM = DRAM[Bit](n)
+    val maskDRAM = DRAM[Int](n)
     val outDRAM = DRAM[Int](n)
     setMem(dataDRAM, data)
     setMem(maskDRAM, mask)
@@ -19,20 +19,22 @@ import spatial.dsl._
       val p = 8
 
       val dataF = FIFO[Int](p)
-      val maskF = FIFO[Bit](p)
+      val maskF = FIFO[Int](p)
       val outF = FIFO[Int](p)
 
       Stream {
         dataF load dataDRAM(0::n par p)
         maskF load maskDRAM(0::n par p)
         Foreach(n par p) { i =>
-          val c = compress(pack(dataF.deq(), maskF.deq()))
+          val m = maskF.deq().bit(0)
+          val c = compress(pack(dataF.deq(), m))
           outF.enq(c._1)
         }
-        outDRAM(0::n/2 par p) store outF
+        outDRAM(0::n par p) store outF
       }
     }
 
-    //println(getArg(out))
+    //println(mask)
+
   }
 }
